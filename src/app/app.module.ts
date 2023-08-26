3f83309f2290b834bfc6dc33215a3da46753e5b5
Darshan Khairnar <darshankhairnar72@gmail.com>
2023-08-26 12:12:20 +0530

    created ui for header and layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProductsListComponent } from './products-list/products-list.component';
-import { ProductComponent } from './product/product.component';
+import { ProductsListComponent } from './Components/products-list/products-list.component'
+import { ProductComponent } from './Components/product/product.component';
+import { HeaderComponent } from './Layout/header/header.component';
+import { FooterComponent } from './Layout/footer/footer.component';
+import { MainComponent } from './Layout/main/main.component';
+import { ProductsFilterComponent } from './Components/products-filter/products-filter.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductsListComponent,
-    ProductComponent
+    ProductComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent,
+    ProductsFilterComponent
   ],
   imports: [
     BrowserModule,
