17ba03b76066e776e412737e4fef32a9441040af
Darshan Khairnar <darshankhairnar72@gmail.com>
2023-09-06 20:18:27 +0530

    working on filter

--- src/app/Components/products-filter/products-filter.component.ts
+++ src/app/Components/products-filter/products-filter.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-products-filter',
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./products-filter.component.css']
 })
 export class ProductsFilterComponent {
+
+
+  choiceCategory = 'all';
+
+
+  @Output()
+  changedEvent:EventEmitter<string> = new EventEmitter<string>();
+
+
+  onChangeCategory(){
+    console.log(this.choiceCategory);
+    this.changedEvent.emit(this.choiceCategory);
+  }
 
 }
--- src/app/Components/products-list/products-list.component.ts
+++ src/app/Components/products-list/products-list.component.ts
@@ -1,27 +1,72 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ProductService } from 'src/app/Services/product.service';
 import * as productData from '../../../assets/product.json';
-
 
 @Component({
   selector: 'app-products-list',
   templateUrl: './products-list.component.html',
   styleUrls: ['./products-list.component.css'],
 })
-export class ProductsListComponent implements OnInit{
+export class ProductsListComponent implements OnInit {
+  constructor(private productService: ProductService) {}
 
-  constructor(private productService:ProductService){}
+  @Input()
+  category!: string;
 
-  products:any;
-  searchQuery = '';
+  temp: any;
+  allProducts: any[] = [];
+  newAllPro: any[] = [];
+  query: string = '';
 
   ngOnInit(): void {
-    console.log(productData);
-    this.productService.getJsonData().subscribe(data => {
-      this.products = data;
-    })
-    
+    this.productService.getJsonData().subscribe((data) => {
+      this.temp = data;
+      console.log(this.temp);
+      this.temp.forEach((p: any) => {
+        this.allProducts.push(p);
+      });
+    });
   }
-  
 
+  changeInput(event: any) {
+    let keyword = event.target.value;
+    this.allProducts = [];
+
+    this.productService.getJsonData().subscribe((data) => {
+      this.temp = data;
+      this.temp.forEach((p: any) => {
+        if(p.name.substring(keyword)){
+          this.allProducts.push(p);
+        }
+      });
+    });
+
+    console.log(this.allProducts);
+    console.log(keyword);
+
+
+  }
+
+  onCatgChange(value: string) {
+    this.category = value;
+    console.log(this.category);
+    this.allProducts = [];
+    if (this.category != 'all') {
+      this.productService.getJsonData().subscribe((data) => {
+        this.temp = data;
+        this.temp.forEach((p: any) => {
+          if (p.category == this.category) {
+            this.allProducts.push(p);
+          }
+        });
+      });
+    } else {
+      this.productService.getJsonData().subscribe((data) => {
+        this.temp = data;
+        this.temp.forEach((p: any) => {
+          this.allProducts.push(p);
+        });
+      });
+    }
+  }
 }
